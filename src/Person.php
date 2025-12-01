4dfc8c1aac7b211dc5575cd34deb89c56d39f75a
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-01 17:55:46 +0900

    Add comment

--- src/Person.php
+++ src/Person.php
@@ -5,11 +5,13 @@ namespace Hidenari\ModelSample;
 class Person
 {
     public function __construct(
+        // all public pattern
         public string $forename
         = 'taro' {
             get => 'Mr.'.$this->forename;
             set => $this->forename = ucwords($value);
         },
+        // set private get public pattern
         public private(set) string $surname
         = 'yamada' {
             get => 'Mr.'.$this->surname;
