3c2b95002b755570ab5830f4a162c177020f833a
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-04 13:15:39 +0900

    Add not test parturn

--- tests/Unit/PersonCapsuleTest.php
+++ tests/Unit/PersonCapsuleTest.php
@@ -5,11 +5,13 @@ use Hidenari\ModelSample\PersonCapsule;
 test('person capsule model no parameter', function () {
     $person = new PersonCapsule();
     expect($person->name == 'Mr.taro')->toBeTrue();
+    expect($person->name == 'taro')->toBeFalse();
     expect($person->age == 15)->toBeTrue();
 });
 
 test('person capsule with name age parameter', function () {
     $person = new PersonCapsule('jiro',20);
     expect($person->name == 'Mr.jiro')->toBeTrue();
+    expect($person->name == 'jiro')->toBeFalse();
     expect($person->age == 20)->toBeTrue();
 });
--- tests/Unit/PersonTest.php
+++ tests/Unit/PersonTest.php
@@ -5,11 +5,15 @@ use Hidenari\ModelSample\Person;
 test('person model no parameter', function () {
     $person = new Person();
     expect($person->name == 'Mr.Taro')->toBeTrue();
+    expect($person->name == 'Taro')->toBeFalse();
+    expect($person->name == 'taro')->toBeFalse();
     expect($person->age == 15)->toBeTrue();
 });
 
 test('person model with name age parameter', function () {
     $person = new Person('jiro',20);
     expect($person->name == 'Mr.Jiro')->toBeTrue();
+    expect($person->name == 'Jiro')->toBeFalse();
+    expect($person->name == 'jiro')->toBeFalse();
     expect($person->age == 20)->toBeTrue();
 });
