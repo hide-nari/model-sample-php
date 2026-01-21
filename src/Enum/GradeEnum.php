2415351218b06dff309f8ff78806d38473da1968
watanabeHi <hide3@mild.ocn.ne.jp>
2026-01-22 08:35:09 +0900

    Add Enum function

--- src/Enum/GradeEnum.php
+++ src/Enum/GradeEnum.php
@@ -7,4 +7,22 @@ enum GradeEnum: int
     case GOLD = 3;
     case SILVER = 2;
     case BRONZE = 1;
+
+    public function upGrade($grade)
+    {
+        return match ($grade) {
+            GradeEnum::BRONZE => GradeEnum::SILVER,
+            GradeEnum::SILVER => GradeEnum::GOLD,
+            default => throw new \InvalidArgumentException('Unexpected Enum value'),
+        };
+    }
+
+    public function downGrade($grade)
+    {
+        return match ($grade) {
+            GradeEnum::GOLD => GradeEnum::SILVER,
+            GradeEnum::SILVER => GradeEnum::BRONZE,
+            default => throw new \InvalidArgumentException('Unexpected Enum value'),
+        };
+    }
 }
